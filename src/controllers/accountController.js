e51527072ecd7c14971055f75afa76232178711a
Tzu-Chao Wang <john29917958@gmail.com>
2024-10-05 15:26:41 +0800

    Add sign up view and controller actions

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -1,5 +1,11 @@
 const asyncHandler = require("express-async-handler");
 
+exports.signIn = asyncHandler(async (req, res) => {
+  res.render("pages/account/signIn", { title: "Sign-in" });
+});
+
+exports.auth = asyncHandler(async (req, res) => {});
+
 exports.signUp = asyncHandler(async (req, res) => {
   res.render("pages/account/signUp", { title: "Sign-up" });
 });
--- src/routes/account.js
+++ src/routes/account.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const accountController = require("../controllers/accountController");
 
+router.get("/sign-in", accountController.signIn);
+router.get("/auth", accountController.auth);
 router.get("/sign-up", accountController.signUp);
 router.post("/store", accountController.store);
 
